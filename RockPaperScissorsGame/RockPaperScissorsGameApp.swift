d8b3969ef278bf2506d31a4b4935daea151681c4
George-Hachem <78202573+George-H12@users.noreply.github.com>
2023-04-21 01:55:00 -0400

    Initial Commit

---
+++ RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by George on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
